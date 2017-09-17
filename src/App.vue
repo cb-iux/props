59fc1e83baacc805e9ee1dfd8102d2f49e7524f6
chris-work <cb@iux.no>
2017-09-17 16:41:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <app-styleone v-bind:content="content"></app-styleone>
+</template>
+
+<script>
+import styleone from './components/styleone'
+
+export default {
+  components: {
+    'app-styleone': styleone
+  },
+  data () {
+    return {
+      content: {
+        title: 'Christoffer Bogsti'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/styleone.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <h1>{{ content.title }}</h1>
+</template>
+
+<script>
+export default {
+  props: ['content']
+}
+</script>
+
+<style lang="css">
+</style>
