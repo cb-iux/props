2179ebc2bc884870dc06545e359b5f550b07ad1d
chris-work <cb@iux.no>
2017-09-17 20:21:39 +0200

    three comps

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template lang="html">
-  <app-styleone v-bind:content="content"></app-styleone>
+  <div>
+    <app-styleone v-bind:content="content" v-if="content.index === 1"></app-styleone>
+    <app-styletwo v-bind:content="content" v-if="content.index === 2"></app-styletwo>
+    <app-stylethree v-bind:content="content" v-if="content.index === 3"></app-stylethree>
+    <button v-on:click="numcount">NEXT</button>
+  </div>
 </template>
 
 <script>
 import styleone from './components/styleone'
+import styletwo from './components/styletwo'
+import stylethree from './components/stylethree'
 
 export default {
   components: {
-    'app-styleone': styleone
+    'app-styleone': styleone,
+    'app-styletwo': styletwo,
+    'app-stylethree': stylethree
   },
   data () {
     return {
       content: {
+        index: 1,
+        styles: 3,
         title: 'Christoffer Bogsti'
       }
+    }
+  },
+  methods: {
+    numcount: function () {
+      if (this.content.index === this.content.styles) {
+        this.content.index = 0
+      }
+      this.content.index++
+      console.log(this.content.index)
     }
   }
 }
 </script>
 
-<style lang="css">
+<style lang="sass">
 </style>
--- src/components/stylethree.vue
+++ src/components/stylethree.vue
@@ -9,4 +9,7 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+  h1 
+    color: blue
+
 </style>
