d9a8f01f028388c8960dd86207ff6ce153288715
chris-work <cb@iux.no>
2017-10-17 18:33:28 +0200

    added agile

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,12 @@ export default {
 </script>
 
 <style lang="sass">
+  html, body
+    width: 100%
+    margin: 0
+  //
+  // html, body, #app
+  //   height: 100%
+  //   #portfolio, .agile, .agile__list, .agile__track
+  //     height: 100%
 </style>
--- src/views/portfolio.vue
+++ src/views/portfolio.vue
@@ -1,13 +1,19 @@
 <template lang="html">
   <div id="portfolio">
-    YOYOYO
+    <compBusemannen></compBusemannen>
   </div>
 </template>
 
 <script>
+import Busemannen from '../components/portfolioBusemannen.vue'
+
 export default {
+  components: {
+    'compBusemannen': Busemannen
+  }
 }
 </script>
 
 <style lang="sass" scoped>
+
 </style>
